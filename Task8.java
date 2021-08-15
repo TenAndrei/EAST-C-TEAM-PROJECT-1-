b116c5cd06511e084e4eb9b7aaa6b76dedc6600f
TenAndrei <fcbfanat@gmail.com>
2021-08-15 11:58:56 -0400

    renamed the file

--- Task8.java
+++ Task8.java
@@ -27,6 +27,33 @@ public class Task8 {
 	    }
 	
 	    System.out.println("\nSecond largest number is:" + secondLargest);
+	    int array_variable[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+	    int sum = 0;
+
+	    for (int i = 0; i < 3; ++i) {
+
+	       for (int j = 0; j < 3; ++j) {
+
+	           sum = sum + array_variable[i][j];
+
+	       }
+
+	    }
+
+	    System.out.print(sum / 5);
+	    
+	    int m=2, total=0;
+
+	    while(m<6) {
+
+	       total=total+m;
+
+	       m++;
+
+	    }
+
+	    System.out.print(total);
 	
 	}
 }
